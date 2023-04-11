55ce21576702c11f68da56f06b1940a4dada787d
Alger125 <lucianojimenez662@gmail.com>
2023-04-11 14:25:51 -0600

    "CAMBIANDO SYSO POR LOG EN APP"

--- src/main/java/com/bbva/arqspring/App.java
+++ src/main/java/com/bbva/arqspring/App.java
@@ -3,6 +3,7 @@ package com.bbva.arqspring;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 
 import com.bbva.arqspring.CargaMasiva.service.CatalogoContratosService;
@@ -14,8 +15,11 @@ import com.bbva.arqspring.util.Config;
  *
  */
 public class App {
+	
+	private static Logger LogJava = Logger.getLogger(App.class);
+	
 	public static void main(String[] args) {
-
+		LogJava.debug("INICIO");
 		Config.setNumberProcess(207);
 		Config.setNoActivo(NoActiveProcess.class);
 		ApplicationContext c = Config.obtenerContexto();
@@ -29,14 +33,14 @@ public class App {
 			long diferencia = fecha2.getTime() - fecha1.getTime();
 			long segundos = TimeUnit.MILLISECONDS.toSeconds(diferencia);
 			long minutos = TimeUnit.MILLISECONDS.toMinutes(diferencia);
-			System.err.println("Tiempo de ejecucion ====  " + segundos + " segundos ");
-			System.err.println("Tiempo de ejecucion ====  " + minutos + " minutos ");
-			System.err.println("FIN DE EJECUCION");
+			LogJava.debug("Tiempo de ejecucion ====  " + segundos + " segundos ");
+			LogJava.debug("Tiempo de ejecucion ====  " + minutos + " minutos ");
+			LogJava.debug("FIN DE EJECUCION");
 
 			System.exit(0);
 		} catch (Exception e) {
 			String messageOutPut = "Error al ejecutar proceso";
-			System.err.println("Error: " + messageOutPut);
+			LogJava.debug("Error: " + messageOutPut);
 			System.exit(1);
 		}
 
