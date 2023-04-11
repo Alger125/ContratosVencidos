31d48d113a714cd85ff8e3b651f07571431352b5
Alger125 <lucianojimenez662@gmail.com>
2023-04-11 12:53:02 -0600

    "Arreglando clase App"

--- src/main/java/com/bbva/arqspring/App.java
+++ src/main/java/com/bbva/arqspring/App.java
@@ -20,7 +20,7 @@ public class App {
 		Config.setNumberProcess(207);
 		Config.setNoActivo(NoActiveProcess.class);
 		ApplicationContext c = Config.obtenerContexto();
-		System.out.println("ACTIVO");
+		System.err.println("ACTIVO");
 		CatalogoContratosService catalogo = c.getBean(CatalogoContratosService.class);
 
 		try {
@@ -30,14 +30,14 @@ public class App {
 			long diferencia = fecha2.getTime() - fecha1.getTime();
 			long segundos = TimeUnit.MILLISECONDS.toSeconds(diferencia);
 			long minutos = TimeUnit.MILLISECONDS.toMinutes(diferencia);
-			System.out.println("Tiempo de ejecucion ====  " + segundos + " segundos ");
-			System.out.println("Tiempo de ejecucion ====  " + minutos + " minutos ");
-			System.out.println("FIN DE EJECUCION");
-			System.out.println("jkfhjdkhfjkdhfjkd");
+			System.err.println("Tiempo de ejecucion ====  " + segundos + " segundos ");
+			System.err.println("Tiempo de ejecucion ====  " + minutos + " minutos ");
+			System.err.println("FIN DE EJECUCION");
+			
 			System.exit(0);
 		} catch (Exception e) {
 			String messageOutPut = "Error al ejecutar proceso";
-			System.out.println("Error: " + messageOutPut);
+			System.err.println("Error: " + messageOutPut);
 			System.exit(1);
 		}
 		
