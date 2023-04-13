8700737782b1e135bdae0e2b7a40b23c47c4ea7f
Alger125 <lucianojimenez662@gmail.com>
2023-04-13 13:18:51 -0600

    public boolean validarCorreo

--- src/main/java/cargamasiva/dao/impl/ContratosDAOImpl.java
+++ src/main/java/cargamasiva/dao/impl/ContratosDAOImpl.java
@@ -59,7 +59,7 @@ public class ContratosDAOImpl extends BaseDAO implements ContratosDAO {
 					FideicomisarioDTO ctoFideicomisario = new FideicomisarioDTO();
 					ctoFideicomisario.setFechaVencimiento((java.sql.Date) query.get("FECHA_VENCIMIENTO"));
 					ctoFideicomisario.setCveTipoNegocio((String) query.get("CTO_CVE_TIPO_NEG"));
-					if (validarCorreo((String) query.get("BEN_E_MAIL")) == true) {
+					if (validarCorreo((String) query.get("BEN_E_MAIL"))) {
 						ctoFideicomisario.setCorreo((String) query.get("BEN_E_MAIL"));
 						formateado = String.format("%-300s", query.get("BEN_E_MAIL"));
 						ctoFideicomisario.setCorreo(formateado);
