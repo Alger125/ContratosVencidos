57a8ed0564cecd78a307a70779d460958b6169b8
Alger125 <lucianojimenez662@gmail.com>
2023-04-14 11:26:21 -0600

    "Arreglando esto"

--- src/main/java/cargamasiva/dao/impl/ContratosDAOImpl.java
+++ src/main/java/cargamasiva/dao/impl/ContratosDAOImpl.java
@@ -131,10 +131,11 @@ public class ContratosDAOImpl extends BaseDAO implements ContratosDAO {
 		
 		Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)(\\.[A-Za-z]{2,})$");
         Matcher mather = pattern.matcher(email.trim());
-        if(mather.equals(true)) {
-        	return mather;
+       
+        if (mather.find()) {
+        return true;
+        }  {
+        return false;
         }
-		return false;
-        
 	}
 }
